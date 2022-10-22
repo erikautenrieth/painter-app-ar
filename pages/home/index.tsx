5d2209b43fb7cfc7b9292ce26c691c8c33ef5951
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-10-22 23:26:18 +0200

    get user after login

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,3 +1,8 @@
+import { useAuth } from "../../shared-components/services/auth-context";
+
 export default function Home() {
+  const { user } = useAuth();
+  console.log("hamedkabir   ", user);
+
   return <h1>Page Home</h1>;
 }
