740c9a8eed6accc742e5e027c164f7149cf01175
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-10-25 22:15:33 +0200

    get ready Buttons and interactions

--- pages/hosting-page/[hostingId].tsx
+++ pages/hosting-page/[hostingId].tsx
@@ -1,11 +1,17 @@
-import { Grid } from "@mui/material";
+import { Button, Grid, Icon } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import CheckCircleOutlineOutlinedIcon from "@mui/icons-material/CheckCircleOutlineOutlined";
 
 const HostingPage = () => {
   const router = useRouter();
   const [userRole, setUserRole] = useState<any>("admin");
+  const [userState, setUserState] = useState<any>(false);
   const { hostingId } = router.query;
+
+  const getReady = () => {
+    setUserState(true);
+  };
   useEffect(() => {
     setUserRole(hostingId);
   }, []);
@@ -16,9 +22,39 @@ const HostingPage = () => {
       <Grid container spacing={{ md: 3 }} columns={{ md: 12 }}>
         <Grid item xs={6}>
           <h1>Player1</h1>
+          {userRole == "admin" ? (
+            <>
+              {userState ? (
+                <Icon
+                  color="success"
+                  fontSize="large"
+                  component={CheckCircleOutlineOutlinedIcon}
+                ></Icon>
+              ) : (
+                <Button variant="outlined" onClick={() => getReady()}>
+                  Ready
+                </Button>
+              )}
+            </>
+          ) : null}
         </Grid>
         <Grid item xs={6}>
           <h1>Player2</h1>
+          {userRole == "join" ? (
+            <>
+              {userState ? (
+                <Icon
+                  color="success"
+                  fontSize="large"
+                  component={CheckCircleOutlineOutlinedIcon}
+                ></Icon>
+              ) : (
+                <Button variant="outlined" onClick={() => getReady()}>
+                  Ready
+                </Button>
+              )}
+            </>
+          ) : null}
         </Grid>
       </Grid>
     </>
