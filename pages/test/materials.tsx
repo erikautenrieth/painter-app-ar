97e557be7c0756a4769dd62808b0836d0549e1e4
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-11-02 13:59:41 +0100

    textures training

---
+++ pages/test/materials.tsx
@@ -0,0 +1,38 @@
+import { Canvas } from "@react-three/fiber";
+import { NextPage } from "next";
+import { OrbitControls, useTexture } from "@react-three/drei";
+const TexturedSpheres = () => {
+  const props = useTexture({
+    map: "/textures/rocky_trail_diff_1k.png",
+    displacementMap: "/textures/rocky_trail_disp_1k.png",
+    normalMap: "/textures/rocky_trail_nor_gl_1k.png",
+    roughnessMap: "/textures/rocky_trail_rough_1k.png",
+  });
+
+  return (
+    <>
+      <mesh scale={[1, 1, 1]} position={[0, 1, 0]}>
+        <sphereGeometry args={[1, 100, 100]}></sphereGeometry>
+        <meshStandardMaterial
+          {...props}
+          displacementScale={0.1}
+        ></meshStandardMaterial>
+      </mesh>
+    </>
+  );
+};
+
+const Materials: NextPage = () => {
+  return (
+    <div className="container">
+      <Canvas>
+        <gridHelper args={[10, 10]}></gridHelper>
+        <OrbitControls></OrbitControls>
+        <ambientLight intensity={0.3}></ambientLight>
+        <directionalLight position={[0, 5, 5]}></directionalLight>
+        <TexturedSpheres></TexturedSpheres>
+      </Canvas>
+    </div>
+  );
+};
+export default Materials;
