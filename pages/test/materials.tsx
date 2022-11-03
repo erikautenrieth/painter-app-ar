90081875fc79df8cf637d435e50704ec2ee48f75
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-11-03 15:38:03 +0100

    camera control

--- pages/test/materials.tsx
+++ pages/test/materials.tsx
@@ -1,61 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { NextPage } from "next";
-import {
-  OrbitControls,
-  useTexture,
-  TransformControls,
-  useGLTF,
-  useAnimations,
-} from "@react-three/drei";
+import { OrbitControls, useTexture } from "@react-three/drei";
 import Lights from "./components/Lights";
 import Ground from "./components/Ground";
 import PalmModel from "./components/Palm";
 import Palm2 from "./components/PalmV2";
-import { useEffect, useRef } from "react";
-import { MeshDistanceMaterial } from "three";
-import { useInput } from "./hooks/useInput";
-
-const MyPlayer = () => {
-  const { forward, backward, left, right, jump, shift } = useInput();
-  const model = useGLTF("/models/player.glb");
-  const { actions } = useAnimations(model.animations, model.scene);
-  model.scene.traverse((object) => {
-    if (object.isMesh) {
-      object.castShadow = true;
-    }
-  });
-  const currentAction = useRef("");
-  // animations dance, idle, jump, running
-  useEffect(() => {
-    let action = "";
-
-    if (forward || backward || left || right) {
-      action = "running";
-    } else if (jump) {
-      action = "jump";
-    } else if (shift) {
-      action = "dance";
-    } else {
-      action = "idle";
-    }
-    // actions?.dance?.play();
-    // console.log("forward  ", forward);
-    // console.log("backward  ", backward);
-    // console.log("left   ", left);
-    // console.log("right   ", right);
-    // console.log("jump   ", jump);
-    // console.log("shift   ", shift);
-    if (currentAction.current != action) {
-      const nextActionToPlay = actions[action];
-      const current = actions[currentAction.current];
-      current?.fadeOut(0.2);
-      nextActionToPlay?.reset().fadeIn(0.2).play();
-      currentAction.current = action;
-    }
-  }, [forward, backward, left, right, jump, shift]);
-
-  return <primitive object={model.scene}></primitive>;
-};
+import MyPlayer from "./components/Player";
 
 const TexturedSpheres = () => {
   const props = useTexture({
