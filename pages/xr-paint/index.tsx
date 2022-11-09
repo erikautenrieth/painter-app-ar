02bc5bebca191b5801d8afcbb3af4b8ccaf01e4b
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-11-09 11:04:12 +0100

    created a painter component

--- pages/xr-paint/index.tsx
+++ pages/xr-paint/index.tsx
@@ -1,74 +1,11 @@
 import { PerspectiveCamera } from "@react-three/drei";
-import { Canvas, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { ARButton, XR } from "@react-three/xr";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { TubePainter } from "three/examples/jsm/misc/TubePainter.js";
-const Painter = () => {
-  const { gl, scene, camera, size } = useThree();
-  let controller: any;
-  let painter: any;
-  const cursor = new THREE.Vector3();
-  const [userDataSelecting, setUserDataSelecting] = useState<boolean>(false);
-  const init = () => {
-    gl.setPixelRatio(window.devicePixelRatio);
-    gl.setSize(window.innerWidth, window.innerHeight);
+import Painter1 from "./painter1";
 
-    painter = new TubePainter();
-    painter.setSize(0.4);
-    painter.mesh.material.side = THREE.DoubleSide;
-    scene.add(painter.mesh);
 
-    controller = gl.xr.getController(0);
-    controller.addEventListener("selectstart", onSelectStart);
-    controller.addEventListener("selectend", onSelectEnd);
-    controller.userData.skipFrames = 0;
-    scene.add(controller);
-    function onSelectStart(this: any) {
-      this.userData.isSelecting = true;
-      this.userData.skipFrames = 2;
-      setUserDataSelecting(true);
-    }
-
-    function onSelectEnd(this: any) {
-      this.userData.isSelecting = false;
-      setUserDataSelecting(false);
-    }
-  };
-
-  const handleController = (ctl?: any) => {
-    if (controller) {
-      const userData = ctl.userData;
-      cursor.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
-
-      if (userDataSelecting) {
-        if (userData.skipFrames >= 0) {
-          // TODO(mrdoob) Revisit thi
-
-          userData.skipFrames--;
-
-          painter.moveTo(cursor);
-        } else {
-          painter.lineTo(cursor);
-          painter.update();
-        }
-      }
-    }
-  };
-  useEffect(() => {
-    init();
-    animate();
-  }, [userDataSelecting]);
-
-  function animate() {
-    gl.setAnimationLoop(render);
-  }
-  function render() {
-    handleController(controller);
-    gl.render(scene, camera);
-  }
-  return <mesh></mesh>;
-};
 const PaintXR = () => {
   const cameraRef = useRef<THREE.PerspectiveCamera>(null);
   const lightRef = useRef<THREE.HemisphereLight>(null);
@@ -102,7 +39,9 @@ const PaintXR = () => {
             args={["0xffffff", "0xbbbbff", 1]}
             ref={lightRef}
           ></hemisphereLight>
-          <Painter></Painter>
+          {/* <Painter></Painter> */}
+
+          <Painter1></Painter1>
         </XR>
       </Canvas>
     </div>
--- pages/xr-paint/painter1.tsx
+++ pages/xr-paint/painter1.tsx
@@ -1,4 +1,71 @@
+import { useThree } from "@react-three/fiber";
+import { useEffect, useState } from "react";
+import * as THREE from "three"
+import { TubePainter } from "three/examples/jsm/misc/TubePainter.js";
+
 const Painter1: React.FC = () => {
+  const { gl, scene, camera, size } = useThree();
+  let controller: any;
+  let painter: any;
+  const cursor = new THREE.Vector3();
+  const [userDataSelecting, setUserDataSelecting] = useState<boolean>(false);
+  const init = () => {
+    gl.setPixelRatio(window.devicePixelRatio);
+    gl.setSize(window.innerWidth, window.innerHeight);
+
+    painter = new TubePainter();
+    painter.setSize(0.4);
+    painter.mesh.material.side = THREE.DoubleSide;
+    scene.add(painter.mesh);
+
+    controller = gl.xr.getController(0);
+    controller.addEventListener("selectstart", onSelectStart);
+    controller.addEventListener("selectend", onSelectEnd);
+    controller.userData.skipFrames = 0;
+    scene.add(controller);
+    function onSelectStart(this: any) {
+      this.userData.isSelecting = true;
+      this.userData.skipFrames = 2;
+      setUserDataSelecting(true);
+    }
+
+    function onSelectEnd(this: any) {
+      this.userData.isSelecting = false;
+      setUserDataSelecting(false);
+    }
+  };
+
+  const handleController = (ctl?: any) => {
+    if (controller) {
+      const userData = ctl.userData;
+      cursor.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
+
+      if (userDataSelecting) {
+        if (userData.skipFrames >= 0) {
+          // TODO(mrdoob) Revisit thi
+
+          userData.skipFrames--;
+
+          painter.moveTo(cursor);
+        } else {
+          painter.lineTo(cursor);
+          painter.update();
+        }
+      }
+    }
+  };
+  useEffect(() => {
+    init();
+    animate();
+  }, [userDataSelecting]);
+
+  function animate() {
+    gl.setAnimationLoop(render);
+  }
+  function render() {
+    handleController(controller);
+    gl.render(scene, camera);
+  }
   return <></>;
 };
 
