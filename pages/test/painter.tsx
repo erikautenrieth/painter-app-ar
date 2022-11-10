a7e35ebf7bc6b4c0d59d0c844e15b8a86a47f5bf
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-11-10 20:40:18 +0100

    two painter and two controller

--- pages/test/painter.tsx
+++ pages/test/painter.tsx
@@ -6,7 +6,7 @@ import { ARButton } from "three/examples/jsm/webxr/ARButton.js";
 const Tube_Painter = () => {
   let container;
   let camera: any, scene: any, renderer: any;
-  let controller: any, painter: any;
+  let controller1: any, controller2: any, painter1: any, painter2: any;
   const cursor = new THREE.Vector3();
   useEffect(() => {
     init();
@@ -51,12 +51,17 @@ const Tube_Painter = () => {
 
     //
 
-    painter = new TubePainter();
-    painter.setSize(0.6);
-    painter.mesh.material.side = THREE.DoubleSide;
-    painter.mesh.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    scene.add(painter.mesh);
+    painter1 = new TubePainter();
+    painter1.setSize(0.6);
+    painter1.mesh.material.side = THREE.DoubleSide;
+    painter1.mesh.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    scene.add(painter1.mesh);
 
+    painter2 = new TubePainter();
+    painter2.setSize(0.6);
+    painter2.mesh.material.side = THREE.DoubleSide;
+    painter2.mesh.material = new THREE.MeshBasicMaterial({ color: 0x08f3ff });
+    scene.add(painter2.mesh);
     //
 
     function onSelectStart(this: any) {
@@ -68,11 +73,19 @@ const Tube_Painter = () => {
       this.userData.isSelecting = false;
     }
 
-    controller = renderer.xr.getController(0);
-    controller.addEventListener("selectstart", onSelectStart);
-    controller.addEventListener("selectend", onSelectEnd);
-    controller.userData.skipFrames = 0;
-    scene.add(controller);
+    controller1 = renderer.xr.getController(0);
+    controller1.addEventListener("selectstart", onSelectStart);
+    controller1.addEventListener("selectend", onSelectEnd);
+    controller1.userData.skipFrames = 0;
+    controller1.userData.painter = painter1;
+    scene.add(controller1);
+
+    controller2 = renderer.xr.getController(0);
+    controller2.addEventListener("selectstart", onSelectStart);
+    controller2.addEventListener("selectend", onSelectEnd);
+    controller2.userData.skipFrames = 0;
+    controller2.userData.painter = painter2;
+    scene.add(controller2);
 
     //
 
@@ -89,20 +102,23 @@ const Tube_Painter = () => {
   //
 
   function handleController(controller: any) {
-    const userData = controller.userData;
+    if (controller) {
+      const userData = controller.userData;
+      const painter = userData.painter;
 
-    cursor.set(0, 0, -0.2).applyMatrix4(controller.matrixWorld);
+      cursor.set(0, 0, -0.2).applyMatrix4(controller.matrixWorld);
 
-    if (userData.isSelecting === true) {
-      if (userData.skipFrames >= 0) {
-        // TODO(mrdoob) Revisit this
+      if (userData.isSelecting === true) {
+        if (userData.skipFrames >= 0) {
+          // TODO(mrdoob) Revisit this
 
-        userData.skipFrames--;
+          userData.skipFrames--;
 
-        painter.moveTo(cursor);
-      } else {
-        painter.lineTo(cursor);
-        painter.update();
+          painter.moveTo(cursor);
+        } else {
+          painter.lineTo(cursor);
+          painter.update();
+        }
       }
     }
   }
@@ -112,8 +128,8 @@ const Tube_Painter = () => {
   }
 
   function render() {
-    handleController(controller);
-
+    handleController(controller1);
+    handleController(controller2);
     renderer.render(scene, camera);
   }
   return <div id="container-id" className="containerCanva"></div>;
