faf651b593f9e497b82774c63842e3d5054cf552
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-11-10 20:59:58 +0100

    fixed bug in react paint

--- pages/xr-paint/painter1.tsx
+++ pages/xr-paint/painter1.tsx
@@ -10,21 +10,25 @@ const Painter1: React.FC = () => {
   const cursor = new THREE.Vector3();
   const [userDataSelecting, setUserDataSelecting] = useState<boolean>(false);
   const init = () => {
+    if (camera) {
+      camera.fov = 75;
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.near = 0.01;
+      camera.far = 20;
+    }
+
     gl.setPixelRatio(window.devicePixelRatio);
     gl.setSize(window.innerWidth, window.innerHeight);
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    light.position.set(0, 1, 0);
+    scene.add(light);
 
     painter = new TubePainter();
     painter.setSize(0.4);
     painter.mesh.material.side = THREE.DoubleSide;
     painter.mesh.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
     scene.add(painter.mesh);
-
-    controller = gl.xr.getController(0);
-    controller.addEventListener("selectstart", onSelectStart);
-    controller.addEventListener("selectend", onSelectEnd);
-    controller.userData.skipFrames = 0;
-    scene.add(controller);
 
     function onSelectStart(this: any) {
       this.userData.isSelecting = true;
@@ -36,22 +40,39 @@ const Painter1: React.FC = () => {
       this.userData.isSelecting = false;
       setUserDataSelecting(false);
     }
+    controller = gl.xr.getController(0);
+    controller.addEventListener("selectstart", onSelectStart);
+    controller.addEventListener("selectend", onSelectEnd);
+    controller.userData.skipFrames = 0;
+    controller.userData.painter = painter;
+    scene.add(controller);
+
+    window.addEventListener("resize", onWindowResize);
   };
 
-  const handleController = (ctl?: any) => {
-    const userData = ctl.userData;
-    cursor.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-    if (userDataSelecting === true) {
-      if (userData.skipFrames >= 0) {
-        // TODO(mrdoob) Revisit thi
+    gl.setSize(window.innerWidth, window.innerHeight);
+  }
+  const handleController = (ctl: any) => {
+    if (ctl) {
+      const userData = ctl.userData;
+      const painter = userData.painter;
+      cursor.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
 
-        userData.skipFrames--;
+      if (userDataSelecting === true) {
+        if (userData.skipFrames >= -1) {
+          // TODO(mrdoob) Revisit thi
 
-        painter.moveTo(cursor);
-      } else {
-        painter.lineTo(cursor);
-        painter.update();
+          userData.skipFrames--;
+
+          painter.moveTo(cursor);
+        } else {
+          painter.lineTo(cursor);
+          painter.update();
+        }
       }
     }
   };
