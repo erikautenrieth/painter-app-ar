efb99632c2726659dd6073380a8610e76890b3ec
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-11-10 22:10:55 +0100

    2 controller

--- pages/xr-paint/painter1.tsx
+++ pages/xr-paint/painter1.tsx
@@ -6,9 +6,10 @@ import { TubePainter } from "three/examples/jsm/misc/TubePainter.js";
 const Painter1: React.FC = () => {
   const { gl, scene } = useThree();
   let camera: any;
-  let controller: any;
-  let painter: any;
-  const cursor = new THREE.Vector3();
+  let controller1: any, controller2: any;
+  let painter1: any, painter2: any;
+  const cursor1 = new THREE.Vector3();
+  const cursor2 = new THREE.Vector3();
   const [userDataSelecting, setUserDataSelecting] = useState<boolean>(false);
   const init = () => {
     camera = new THREE.PerspectiveCamera(
@@ -25,11 +26,17 @@ const Painter1: React.FC = () => {
     light.position.set(0, 1, 0);
     scene.add(light);
 
-    painter = new TubePainter();
-    painter.setSize(0.4);
-    painter.mesh.material.side = THREE.DoubleSide;
-    painter.mesh.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    scene.add(painter.mesh);
+    painter1 = new TubePainter();
+    painter1.setSize(0.4);
+    painter1.mesh.material.side = THREE.DoubleSide;
+    painter1.mesh.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    scene.add(painter1.mesh);
+
+    painter2 = new TubePainter();
+    painter2.setSize(0.4);
+    painter2.mesh.material.side = THREE.DoubleSide;
+    painter2.mesh.material = new THREE.MeshBasicMaterial({ color: 0xfcba03 });
+    scene.add(painter2.mesh);
 
     function onSelectStart(this: any) {
       this.userData.isSelecting = true;
@@ -41,12 +48,21 @@ const Painter1: React.FC = () => {
       this.userData.isSelecting = false;
       setUserDataSelecting(false);
     }
-    controller = gl.xr.getController(0);
-    controller.addEventListener("selectstart", onSelectStart);
-    controller.addEventListener("selectend", onSelectEnd);
-    controller.userData.skipFrames = 0;
-    controller.userData.painter = painter;
-    scene.add(controller);
+    controller1 = gl.xr.getController(0);
+    controller1.addEventListener("selectstart", onSelectStart);
+    controller1.addEventListener("selectend", onSelectEnd);
+    controller1.userData.skipFrames = 0;
+    controller1.userData.isSelecting = false;
+    controller1.userData.painter = painter1;
+    scene.add(controller1);
+
+    controller2 = gl.xr.getController(0);
+    controller2.addEventListener("selectstart", onSelectStart);
+    controller2.addEventListener("selectend", onSelectEnd);
+    controller2.userData.skipFrames = 0;
+    controller2.userData.isSelecting = false;
+    controller2.userData.painter = painter2;
+    scene.add(controller2);
 
     window.addEventListener("resize", onWindowResize);
   };
@@ -61,17 +77,16 @@ const Painter1: React.FC = () => {
     if (ctl) {
       const userData = ctl.userData;
       const painter = userData.painter;
-      cursor.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
-
+      cursor1.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
       if (userDataSelecting === true) {
-        if (userData.skipFrames >= -1) {
+        if (userData.skipFrames >= -2) {
           // TODO(mrdoob) Revisit thi
 
           userData.skipFrames--;
 
-          painter.moveTo(cursor);
+          painter.moveTo(cursor1);
         } else {
-          painter.lineTo(cursor);
+          painter.lineTo(cursor1);
           painter.update();
         }
       }
@@ -82,8 +97,9 @@ const Painter1: React.FC = () => {
   }, [userDataSelecting]);
 
   useFrame(() => {
-    if (controller) {
-      handleController(controller);
+    if (controller1) {
+      handleController(controller1);
+      handleController(controller2);
       gl.render(scene, camera);
     }
   });
