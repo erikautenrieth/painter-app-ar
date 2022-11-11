db66d99040a82ced07f1a46c61cb7fab458a12aa
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2022-11-11 21:31:16 +0100

    Revert "bug now work eventlistener"
    This reverts commit a626411b7677902dec482939419e563e6a58d323.

--- pages/xr-paint/index.tsx
+++ pages/xr-paint/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@mui/material";
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { ARButton, XR } from "@react-three/xr";
@@ -10,7 +9,6 @@ import Painter1 from "./painter1";
 
 const PaintXR = () => {
   const [players, setPlayers] = useState<any>(null);
-  const [fakePlayer, setFakePlayer] = useState<string>("player1");
   const getHostByPlayer = () => {
     const docKey = "zb5tWRiOArpG0vR5PjO8";
     onSnapshot(doc(database, `host/${docKey}`), (doc) => {
@@ -21,20 +19,14 @@ const PaintXR = () => {
   };
   useEffect(() => {
     getHostByPlayer();
-  }, [fakePlayer]);
-
+  }, []);
   return (
     <div className="containerCanva">
-      <Button onClick={() => setFakePlayer("player1")}>set Player 1</Button>
-      <Button onClick={() => setFakePlayer("player2")}>set Player 2</Button>
       <ARButton></ARButton>
       <Canvas>
         <XR>
           {players ? (
-            <Painter1
-              paintPositionFromDB={players.player1.position}
-              fakePlayer={fakePlayer}
-            ></Painter1>
+            <Painter1 paintPositionFromDB={players.player1.position}></Painter1>
           ) : null}
         </XR>
       </Canvas>
--- pages/xr-paint/painter1.tsx
+++ pages/xr-paint/painter1.tsx
@@ -6,26 +6,17 @@ import * as THREE from "three";
 import { TubePainter } from "three/examples/jsm/misc/TubePainter.js";
 type Props = {
   paintPositionFromDB: any[];
-  fakePlayer: string;
 };
-const Painter1: React.FC<Props> = ({
-  paintPositionFromDB,
-  fakePlayer,
-}: Props) => {
-  // const { scene } = useThree();
-  let camera: any, gl: any, scene: any;
-  let controller: any, controller2: any;
-  let painter: any, painter2: any;
+const Painter1: React.FC<Props> = ({ paintPositionFromDB }: Props) => {
+  const { gl, scene } = useThree();
+  let camera: any;
+  let controller: any;
+  let painter: any;
   const cursor = new THREE.Vector3();
-  const cursor2 = new THREE.Vector3();
   const [userDataSelecting, setUserDataSelecting] = useState<boolean>(false);
-  const [userDataSelecting2, setUserDataSelecting2] = useState<boolean>(false);
-  const [skipFramePlayer1, setSkipFramePlayer1] = useState<number>(0);
-  const [skipFramePlayer2, setSkipFramePlayer2] = useState<number>(0);
+  // const [arrayOfPositions, setArrayOfPositions] = useState<any[]>([]);
   let arrayOfPositions: any[] = paintPositionFromDB;
   const init = () => {
-    scene = new THREE.Scene();
-
     camera = new THREE.PerspectiveCamera(
       70,
       window.innerWidth / window.innerHeight,
@@ -33,10 +24,8 @@ const Painter1: React.FC<Props> = ({
       20
     );
 
-    gl = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     gl.setPixelRatio(window.devicePixelRatio);
     gl.setSize(window.innerWidth, window.innerHeight);
-    console.log("hamedkabir  gl ", gl);
 
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     light.position.set(0, 1, 0);
@@ -48,63 +37,23 @@ const Painter1: React.FC<Props> = ({
     painter.mesh.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     scene.add(painter.mesh);
 
-    painter2 = new TubePainter();
-    painter2.setSize(0.4);
-    painter2.mesh.material.side = THREE.DoubleSide;
-    painter2.mesh.material = new THREE.MeshBasicMaterial({ color: 0xf5b042 });
-    scene.add(painter2.mesh);
-
     function onSelectStart(this: any) {
-      // this.userData.isSelecting = true;
-      // this.userData.skipFrames = 2;
-      console.log("hamedkabir s ", fakePlayer);
-      switch (fakePlayer) {
-        case "player1":
-          setSkipFramePlayer1(2);
-          setUserDataSelecting(true);
-          break;
-
-        case "player2":
-          setSkipFramePlayer2(2);
-          setUserDataSelecting2(true);
-          break;
-
-        default:
-          break;
-      }
+      this.userData.isSelecting = true;
+      this.userData.skipFrames = 2;
+      setUserDataSelecting(true);
     }
 
     function onSelectEnd(this: any) {
-      // this.userData.isSelecting = false;
-
+      this.userData.isSelecting = false;
+      setUserDataSelecting(false);
       // updatePlayerPosition();
-      switch (fakePlayer) {
-        case "player1":
-          setUserDataSelecting(false);
-          break;
-
-        case "player2":
-          setUserDataSelecting(false);
-          break;
-
-        default:
-          break;
-      }
     }
-
     controller = gl.xr.getController(0);
     controller.addEventListener("selectstart", onSelectStart);
     controller.addEventListener("selectend", onSelectEnd);
-    // controller.userData.skipFrames = 0;
-    // controller.userData.painter = painter;
+    controller.userData.skipFrames = 0;
+    controller.userData.painter = painter;
     scene.add(controller);
-
-    controller2 = gl.xr.getController(0);
-    controller2.addEventListener("selectstart", onSelectStart);
-    controller2.addEventListener("selectend", onSelectEnd);
-    // controller2.userData.skipFrames = 0;
-    // controller2.userData.painter = painter2;
-    scene.add(controller2);
 
     window.addEventListener("resize", onWindowResize);
   };
@@ -117,54 +66,27 @@ const Painter1: React.FC<Props> = ({
   }
   const handleController = (ctl: any) => {
     if (ctl) {
-      if (fakePlayer == "player1") {
-        // const userData = ctl.userData;
-        // const painter = userData.painter;
-        cursor.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
+      const userData = ctl.userData;
+      const painter = userData.painter;
+      cursor.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
+      if (userDataSelecting === true) {
+        if (userData.skipFrames >= -2) {
+          // TODO(mrdoob) Revisit thi
 
-        if (userDataSelecting === true) {
-          if (skipFramePlayer1 >= -2) {
-            // TODO(mrdoob) Revisit thi
-            const skipFrameObject = skipFramePlayer1 - 1;
-            // userData.skipFrames--;
-            setSkipFramePlayer1(skipFrameObject);
-            painter.moveTo(cursor);
-          } else {
-            painter.lineTo(cursor);
-            painter.update();
-            const object = {
-              x: cursor.x,
-              y: cursor.y,
-              z: cursor.z,
-            };
-            // arrayOfPositions.push(object);
-          }
-        }
-      } else if (fakePlayer == "player2") {
-        // const userData = ctl.userData;
-        // const painter = userData.painter;
-        cursor2.set(0, 0, -0.2).applyMatrix4(ctl.matrixWorld);
-        if (userDataSelecting2 === true) {
-          if (skipFramePlayer2 >= -2) {
-            // TODO(mrdoob) Revisit thi
+          userData.skipFrames--;
 
-            const skipFrameObject = skipFramePlayer2 - 1;
-            // userData.skipFrames--;
-            setSkipFramePlayer2(skipFrameObject);
-
-            console.log("move", cursor2);
-            painter2.moveTo(cursor2);
-          } else {
-            console.log("linto", cursor2);
-            painter2.lineTo(cursor2);
-            painter2.update();
-            const object = {
-              x: cursor.x,
-              y: cursor.y,
-              z: cursor.z,
-            };
-            // arrayOfPositions.push(object);
-          }
+          console.log("move", cursor);
+          painter.moveTo(cursor);
+        } else {
+          console.log("linto", cursor);
+          painter.lineTo(cursor);
+          painter.update();
+          const object = {
+            x: cursor.x,
+            y: cursor.y,
+            z: cursor.z,
+          };
+          arrayOfPositions.push(object);
         }
       }
     }
@@ -193,10 +115,12 @@ const Painter1: React.FC<Props> = ({
       } else {
         painter.lineTo(cursor);
         painter.update();
+        console.log("painter  ", painter);
       }
       index++;
       paintFromDB();
     } else {
+      console.log("done");
       cursor.set(0, 0, -0.2);
       painter.moveTo(cursor);
     }
@@ -204,40 +128,16 @@ const Painter1: React.FC<Props> = ({
 
   useEffect(() => {
     init();
+
     paintFromDB();
-    animate();
-  }, [userDataSelecting, arrayOfPositions, fakePlayer]);
+  }, [userDataSelecting, arrayOfPositions]);
 
-  // useFrame(() => {
-  //   switch (fakePlayer) {
-  //     case "player1":
-  //       handleController(controller);
-  //       break;
-  //     case "player2":
-  //       handleController(controller2);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  //   gl.render(scene, camera);
-  // });
-  function animate() {
-    gl.setAnimationLoop(render);
-  }
-
-  function render() {
-    switch (fakePlayer) {
-      case "player1":
-        handleController(controller);
-        break;
-      case "player2":
-        handleController(controller2);
-        break;
-      default:
-        break;
+  useFrame(() => {
+    if (controller) {
+      handleController(controller);
+      gl.render(scene, camera);
     }
-    gl.render(scene, camera);
-  }
+  });
   return <></>;
 };
 
