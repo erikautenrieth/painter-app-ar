857e12b3158e1a8ee1b00fd84137802ff8a33364
Hamedkabir <38732428+Hamedkabir@users.noreply.github.com>
2023-02-11 18:18:48 +0100

    model position and ambient light
    Co-Authored-By: Erik Autenrieth <53609156+Mastercheef@users.noreply.github.com>

--- pages/hosting-page/playerpicture.tsx
+++ pages/hosting-page/playerpicture.tsx
@@ -1,39 +1,44 @@
 import React, { useRef, useState, useEffect } from "react";
 import * as THREE from "three";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-import {Canvas, useFrame} from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 const Model = ({ url }: { url: string }) => {
-	const modelRef = useRef<any>();
-	const [model, setModel] = useState<THREE.Object3D | null>(null);
+  const modelRef = useRef<any>();
+  const [model, setModel] = useState<THREE.Object3D | null>(null);
 
-	useEffect(() => {
-		new FBXLoader().load(url, object => {
-			object.scale.set(4, 4, 4);
-			console.log(object.children)
-			// Farbe des Modells ändern
-			object.traverse(function(child) {
-				if (child instanceof THREE.Mesh) {
-					child.material.color.set(0x8b5e3c);
-				}
-			});
-			setModel(object);
-		});
-	}, [url]);
+  useEffect(() => {
+    new FBXLoader().load(url, (object) => {
+      object.scale.set(2.5, 2.5, 2.5);
+      object.position.set(0, -3, 0);
+      console.log(object.children);
+      // Farbe des Modells ändern
+      object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          child.material.color.set(0x8b5e3c);
+        }
+      });
+      setModel(object);
+    });
+  }, [url]);
 
-	useFrame((state: any, delta: number) => {
-	if (modelRef.current) {modelRef.current.rotation.y += delta * 0.5;}
-	});
+  useFrame((state: any, delta: number) => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += delta * 0.5;
+    }
+  });
 
-	return model ? <primitive object={model} ref={modelRef} /> : null;
+  return model ? <primitive object={model} ref={modelRef} /> : null;
 };
 
-const Playerpicture = ({ name }: { name: string}) => {
-	return (
-		<Canvas>
-			<Model url={`/models/${name}.fbx`} />
-		</Canvas>
-	);
+const Playerpicture = ({ name }: { name: string }) => {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[20, 20, 20]} />
+      <Model url={`/models/${name}.fbx`} />
+    </Canvas>
+  );
 };
 
 export default Playerpicture;
